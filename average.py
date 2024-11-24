c28bd45068ee9407ac262bd88b968d5490cab06f
The Coding Explainer <rehman258025@gmail.com>
2024-11-24 16:39:24 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,12 @@
+print("hello this is your computer bot \nEnter your numbers to calculate the percentage \nLets go!")
+
+
+a=int(input("enter marks 1  : "))
+b=int(input("enter marks 2  : "))
+c=int(input("enter marks 3  : "))
+d=int(input("enter marks 4  : "))
+e=int(input("enter marks 5  : "))
+
+total=(a +b +c +d +e)/5
+print(total)
+print("thanks for using me")
